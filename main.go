7ce6f80156eb1df464fbb225decec7361cb805ec
Rizlan Tamima <rizlan@ojekbro.com>
2022-12-06 17:09:38 +0700

    rework to native

--- main.go
+++ main.go
@@ -2,28 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/labstack/echo/v4"
-	"github.com/rizlantamima/golang-web-boilerplate/config"
 )
 
 func init() {
 	fmt.Println(`initiate project`)
 
-}
-
-func main() {
-
-	configuration, err := config.New(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	e := echo.New()
-
-	config.InitRoutes(e)
-
-	portENV := configuration.GetString("APP_PORT", "3000")
-	e.Logger.Fatal(e.Start(":" + portENV))
 }
